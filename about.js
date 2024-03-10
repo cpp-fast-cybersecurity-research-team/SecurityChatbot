57d7ea07191bcd2ae284f9147e66ed0d698e1107
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2024-03-10 03:02:03 -0700

    Basic implementation of the Navigation and About page.

---
+++ about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div>
+            <h2>About the Fast Research Team</h2>
+            <p>This is the conent of our About page.</p>
+        </div>
+    );
+
+}
+
+export default About;
---
+++ navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+
+    return (
+        <ul>
+            <li><Link to="/about">About</Link></li>
+        </ul>
+    )
+
+}
+
+export default Navigation;
