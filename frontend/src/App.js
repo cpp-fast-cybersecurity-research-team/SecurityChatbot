34aefb141ea2d6d2c7c2733ec2e9fa0a7dec0059
jeffreyhoang <148601460+jeffreyhoang@users.noreply.github.com>
2024-03-12 16:34:01 -0700

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,84 +1,11 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import HomePage from './components/HomePage'
+import AboutPage from './components/AboutPage'
 
 function App() {
-  const [data, setData] = useState(null);
-  const [message, setMessage] = useState("message");
-  const [gptResponse, setGPTResponse] = useState("response");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/');  
-        console.log(response)
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const messageSubmit = async (e) => { 
-    e.preventDefault();
-
-    try {
-      const response = await fetch('http://localhost:5000/ask', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          question: message
-        }),
-      });
-
-      if (response.ok) {
-        console.log('Message Sent Successfully!');
-        const data = await response.json();
-
-        console.log(data.gptResponse)
-        setGPTResponse(data.gptResponse)
-      } else {
-        console.error('Failed to send message:', response.statusText);
-        // Handle error
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <div>
-          <p>Data from Flask: {JSON.stringify(data, null, 2)}</p>
-        </div>
-
-        <div>
-          <form onSubmit={messageSubmit}>
-            <label>
-              Message:
-              <input
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </label>
-            <button type="submit">Send Message</button>
-          </form>
-        </div>
-
-        <div>
-          <p>{gptResponse}</p>
-        </div>
-      </header>
-    </div>
+    <AboutPage></AboutPage>
   );
 }
 
