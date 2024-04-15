5dba54939bdb4ac9349a58d8674682b0b5421945
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-04-14 19:56:58 -0700

    created basic media component for images and urls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import TextChannel from './components/TextChannel';
 
+import Media from './components/Media.jsx';
+
 function App() {
   const [data, setData] = useState(null);
 
@@ -21,12 +23,18 @@ function App() {
   }, []);
 
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={'http://localhost:3000/Cybersecurity_Logo.png'} className="App-logo" alt="logo" />
 
+
         <div>
           <p>FAST Cybersecurity Research Team Chatbot</p>
+          <Media icon="https://cppfast.org/wp-content/uploads/2015/05/hdr.png" url="https://campsite.bio/calpolyfast"/>
+          <Media icon="https://cdn.campsite.bio/eyJidWNrZXQiOiJjYW1wc2l0ZS1iaW8tc3RvcmFnZSIsImtleSI6IkNhbFBvbHlGQVNULzk2ZmQ2OGFhLWM4ODEtNGZmYi05YjhjLWEwODliZWNiOWQyMC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMH19fQ==" url="https://campsite.bio/calpolyfast"/>
+          <Media icon="https://raw.githubusercontent.com/cpp-fast-cybersecurity-research-team/SecurityChatbot/371197dd241cd8a11e9065bba8ab9063d382e1c2/frontend/public/Cybersecurity_Logo.png" url="https://campsite.bio/calpolyfast"/>
+        
         </div>
         
         <div>
---
+++ frontend/src/components/Media.jsx
@@ -0,0 +1,14 @@
+/* call the function to pass the url of the icon (local) and then the link */
+import "./Media.css";
+
+function Media({ icon, url }) {
+  return (
+    <>
+      <a href={url}>
+        <img className="sm" src={icon} alt=""></img>
+      </a>
+    </>
+  );
+}
+
+export default Media;
