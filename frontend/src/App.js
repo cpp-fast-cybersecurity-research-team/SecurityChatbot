ce038aba269da903a22d8fa0bc9019e02f68fe8d
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2024-04-19 00:00:52 -0700

    Incorporated media components & removed testing features in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
 import './App.css';
-import HomePage from './components/HomePage'
-import AboutPage from './components/AboutPage'
+import React, { useEffect, useState } from 'react';
+import TextChannel from './components/TextChannel';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/');  
+        console.log(response)
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <AboutPage></AboutPage>
+    <div className="App">
+      <header className="App-header">
+        <img src={'http://localhost:3000/Cybersecurity_Logo.png'} className="App-logo" alt="logo" />
+
+        <div>
+          <p>FAST Cybersecurity Research Team Chatbot</p>
+        </div>
+        
+        <div>
+          <p>Data from Flask: {JSON.stringify(data, null, 2)}</p>
+        </div>
+
+        <TextChannel/>
+        
+      </header>
+    </div>
   );
 }
 
