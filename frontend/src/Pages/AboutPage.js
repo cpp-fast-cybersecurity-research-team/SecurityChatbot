445bf04da1e04e61312ae7ce0c45c8ca8c4a2558
hastiabbasi <143908512+hastiabbasi@users.noreply.github.com>
2024-04-21 18:54:14 -0700

    Readded the About Page files

---
+++ frontend/src/Pages/AboutPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AboutPage.css'
+
+function AboutPage() {
+    return (
+        <div class='about-page-container'>
+            <h2 class="about-page-title">Fast Cyber Security Research Team</h2>
+            <p class="about-page-information">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br></br>
+                <br></br>
+                <br></br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <br></br>
+                <br></br>
+                <br></br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h4 class="about-page-links-and-socials">Links and Socials:</h4>
+            <div class="about-page-images">
+                <img></img>
+                <img></img>
+                <img></img>
+            </div>
+        </div>
+    )
+}
+
+export default AboutPage;
