933a759a2a28927361f077351499149a47c54d13
dmonde13 <127626846+dmonde13@users.noreply.github.com>
2024-04-24 11:00:44 -0700

    Sidebar (#43)
    * Added functionality to manage page state in Sidebar.js and styled Sidebar component in App.css
    
    * Recreated Sidebar.js. Added Sidebar.js,App.js,and App.css files
    
    * Recreated Sidebar.js. Added changed files
    
    ---------
    
    Co-authored-by: Damon Mapinda <d.map27@Damons-MacBook-Pro.local>
    Co-authored-by: BradJKim <69878058+BradJKim@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import Sidebar from './Sidebar';
 import React, { useEffect, useState } from 'react';
 import TextChannel from './components/TextChannel';
+
 
 function App() {
   const [data, setData] = useState(null);
@@ -22,20 +24,24 @@ function App() {
 
   return (
     <div className="App">
+      <Sidebar />
+      
       <header className="App-header">
+
         <img src={'http://localhost:3000/Cybersecurity_Logo.png'} className="App-logo" alt="logo" />
 
-        <div>
-          <p>FAST Cybersecurity Research Team Chatbot</p>
-        </div>
-        
-        {/* <div>
-          <p>Data from Flask: {JSON.stringify(data, null, 2)}</p>
-        </div> */}
+       </header>
 
-        <TextChannel/>
+      <div>
+        <p>FAST Cybersecurity Research Team Chatbot</p>
+      </div>
+
+      {/* <div>
+        <p>Data from Flask: {JSON.stringify(data, null, 2)}</p>
+      </div> */}
+
+      <TextChannel/>
         
-      </header>
     </div>
   );
 }
